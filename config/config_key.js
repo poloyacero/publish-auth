d3c24e1205f11a65038c8a8c1a6b716da546cefa
Gea Dragonaries <akosipoloyacero@gmail.com>
2021-07-28 21:25:00 +0400

    refresh token

--- config/config_key.js
+++ config/config_key.js
@@ -7,13 +7,14 @@ const isProd = NODE_ENV === 'production';
 const { 
   SERVER_PORT, 
   DB_HOST, DB_PORT, DB_NAME, DB_USERNAME, DB_PASSWORD, DB_DIALECT, 
-  JWT_ACCESS_TOKEN_PRIVKEY, JWT_REFRESH_TOKEN_PRIVKEY, JWT_KEYID_ACCESS_TOKEN, JWT_KEYID_REFRESH_TOKEN, JWT_ACCESS_TOKEN_PUBKEY, 
+  JWT_ACCESS_TOKEN_PRIVKEY, JWT_REFRESH_TOKEN_PRIVKEY, JWT_KEYID_ACCESS_TOKEN, JWT_KEYID_REFRESH_TOKEN, JWT_ACCESS_TOKEN_PUBKEY, JWT_REFRESH_TOKEN_PUBKEY, 
   MAGIC_ROUND 
 } = process.env;
 
 const access_token_secret = fs.readFileSync(JWT_ACCESS_TOKEN_PRIVKEY, 'utf-8');
 const refresh_token_secret = fs.readFileSync(JWT_REFRESH_TOKEN_PRIVKEY, 'utf-8');
 const access_token_public = fs.readFileSync(JWT_ACCESS_TOKEN_PUBKEY, 'utf-8');
+const refresh_token_public = fs.readFileSync(JWT_REFRESH_TOKEN_PUBKEY, 'utf-8');
 
 module.exports = {
   SERVER_PORT,
@@ -23,6 +24,7 @@ module.exports = {
   access_token_secret,
   refresh_token_secret,
   access_token_public,
+  refresh_token_public,
   JWT_KEYID_ACCESS_TOKEN,
   JWT_KEYID_REFRESH_TOKEN,
   MAGIC_ROUND
